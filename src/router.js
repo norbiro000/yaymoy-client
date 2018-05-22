c47ec922303141976d69eecfec785476f5e5312d
norbiro000 <boomtingtong@hotmail.com>
2018-05-23 00:03:07 +0700

    -- [Add] TourCardDisplay router

--- src/router.js
+++ src/router.js
@@ -2,10 +2,12 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
 import About from './views/About.vue'
+import TourCardDisplay from './components/TourCardDisplay'
 
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
       path: '/',
@@ -16,6 +18,12 @@ export default new Router({
       path: '/about',
       name: 'about',
       component: About
+    },
+    {
+      path: '/tour',
+      name: 'tour',
+      props: true,
+      component: TourCardDisplay
     }
   ]
 })
